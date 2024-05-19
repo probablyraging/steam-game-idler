1916146f887de44ea6f45fc139adcbda25341de3
ProbablyRaging <probablyraging@gmail.com>
2024-05-20 09:09:27 +1000

    Initial commit

---
+++ FormIdler.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace Idler
+{
+    partial class FormIdler
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormIdler));
+            this.appHeader = new System.Windows.Forms.PictureBox();
+
+            this.SuspendLayout();
+
+            // appHeader
+            this.appHeader.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.appHeader.Location = new System.Drawing.Point(-1, 0);
+            this.appHeader.Name = "appHeader";
+            this.appHeader.Size = new System.Drawing.Size(292, 136);
+            this.appHeader.TabIndex = 0;
+            this.appHeader.TabStop = false;
+
+            // FormIdler
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 136);
+            this.Controls.Add(this.appHeader);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormIdler";
+            this.Load += new System.EventHandler(this.FormIdler_Load);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox appHeader;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿﻿using System;
+using System.Windows.Forms;
+using Steamworks;
+
+namespace Idler
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No Steam app ID was provided.");
+                return;
+            }
+
+            long appId;
+            if (!long.TryParse(args[0], out appId))
+            {
+                Console.WriteLine("Please provide a valid ID (e.g. 221100).");
+                return;
+            }
+
+            Environment.SetEnvironmentVariable("SteamAppId", appId.ToString());
+
+            if (!SteamAPI.Init())
+            {
+                Console.WriteLine("Failed to initialize Steam API.");
+                return;
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormIdler(appId));
+        }
+    }
+}
