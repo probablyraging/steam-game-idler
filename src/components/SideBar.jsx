1ab5ae331b06e2b65bfa8e5a0c5c35a9bab84e61
ProbablyRaging <probablyraging@gmail.com>
2024-06-05 22:46:42 +1000

    Added tooltips to the sidebar (sort bar) to indicate what each button does

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,6 +5,7 @@ import { MdOutlineTimer10Select, MdOutlineTimer3Select } from 'react-icons/md';
 import { MdAvTimer } from 'react-icons/md';
 import { TiHeartFullOutline } from 'react-icons/ti';
 import { FaSignOutAlt } from 'react-icons/fa';
+import { Tooltip } from '@nextui-org/tooltip';
 
 export default function SideBar({ setUserSummary, sortStyle, setSortStyle }) {
     const handleLogout = () => {
@@ -17,96 +18,108 @@ export default function SideBar({ setUserSummary, sortStyle, setSortStyle }) {
         <React.Fragment>
             <div className='flex justify-between flex-col w-[62px] min-h-calc max-h-calc bg-sidebar'>
                 <div className='flex justify-center items-center flex-col'>
-                    <div className='relative flex justify-center items-center w-full h-[62px] hover:bg-sgi cursor-pointer' onClick={() => setSortStyle('a-z')}>
-                        {sortStyle === 'a-z' && (
-                            <motion.div
-                                className='absolute w-full border-r-4 border-white'
-                                initial={{ height: 0 }}
-                                whileInView={{ height: 30 }}
-                                transition={{
-                                    type: 'spring',
-                                    stiffness: 350,
-                                    damping: 18,
-                                }}
-                            ></motion.div>
-                        )}
-                        <TbSortAZ className='text-white' fontSize={32} />
-                    </div>
-                    <div className='relative flex justify-center items-center w-full h-[62px] hover:bg-sgi cursor-pointer' onClick={() => setSortStyle('z-a')}>
-                        {sortStyle === 'z-a' && (
-                            <motion.div
-                                className='absolute w-full border-r-4 border-white'
-                                initial={{ height: 0 }}
-                                whileInView={{ height: 30 }}
-                                transition={{
-                                    type: 'spring',
-                                    stiffness: 350,
-                                    damping: 18,
-                                }}
-                            ></motion.div>
-                        )}
-                        <TbSortZA className='text-white' fontSize={32} />
-                    </div>
-                    <div className='relative flex justify-center items-center w-full h-[62px] hover:bg-sgi cursor-pointer' onClick={() => setSortStyle('1-0')}>
-                        {sortStyle === '1-0' && (
-                            <motion.div
-                                className='absolute w-full border-r-4 border-white'
-                                initial={{ height: 0 }}
-                                whileInView={{ height: 30 }}
-                                transition={{
-                                    type: 'spring',
-                                    stiffness: 350,
-                                    damping: 18,
-                                }}
-                            ></motion.div>
-                        )}
-                        <MdOutlineTimer10Select className='text-white' fontSize={24} />
-                    </div>
-                    <div className='relative flex justify-center items-center w-full h-[62px] hover:bg-sgi cursor-pointer' onClick={() => setSortStyle('0-1')}>
-                        {sortStyle === '0-1' && (
-                            <motion.div
-                                className='absolute w-full border-r-4 border-white'
-                                initial={{ height: 0 }}
-                                whileInView={{ height: 30 }}
-                                transition={{
-                                    type: 'spring',
-                                    stiffness: 350,
-                                    damping: 18,
-                                }}
-                            ></motion.div>
-                        )}
-                        <MdOutlineTimer3Select className='text-white' fontSize={24} />
-                    </div>
-                    <div className='relative flex justify-center items-center w-full h-[62px] hover:bg-sgi cursor-pointer' onClick={() => setSortStyle('recent')}>
-                        {sortStyle === 'recent' && (
-                            <motion.div
-                                className='absolute w-full border-r-4 border-white'
-                                initial={{ height: 0 }}
-                                whileInView={{ height: 30 }}
-                                transition={{
-                                    type: 'spring',
-                                    stiffness: 350,
-                                    damping: 18,
-                                }}
-                            ></motion.div>
-                        )}
-                        <MdAvTimer className='text-white' fontSize={24} />
-                    </div>
-                    <div className='relative flex justify-center items-center w-full h-[62px] hover:bg-sgi cursor-pointer' onClick={() => setSortStyle('fav')}>
-                        {sortStyle === 'fav' && (
-                            <motion.div
-                                className='absolute w-full border-r-4 border-white'
-                                initial={{ height: 0 }}
-                                whileInView={{ height: 30 }}
-                                transition={{
-                                    type: 'spring',
-                                    stiffness: 350,
-                                    damping: 18,
-                                }}
-                            ></motion.div>
-                        )}
-                        <TiHeartFullOutline className='text-white' fontSize={24} />
-                    </div>
+                    <Tooltip closeDelay={0} placement='left' offset={-7} className='text-xs font-bold' content='Alphabetically Ascending'>
+                        <div className='relative flex justify-center items-center w-full h-[62px] hover:bg-sgi cursor-pointer' onClick={() => setSortStyle('a-z')}>
+                            {sortStyle === 'a-z' && (
+                                <motion.div
+                                    className='absolute w-full border-r-4 border-white'
+                                    initial={{ height: 0 }}
+                                    whileInView={{ height: 30 }}
+                                    transition={{
+                                        type: 'spring',
+                                        stiffness: 350,
+                                        damping: 18,
+                                    }}
+                                ></motion.div>
+                            )}
+                            <TbSortAZ className='text-white' fontSize={32} />
+                        </div>
+                    </Tooltip>
+                    <Tooltip closeDelay={0} placement='left' offset={-7} className='text-xs font-bold' content='Alphabetically Descending'>
+                        <div className='relative flex justify-center items-center w-full h-[62px] hover:bg-sgi cursor-pointer' onClick={() => setSortStyle('z-a')}>
+                            {sortStyle === 'z-a' && (
+                                <motion.div
+                                    className='absolute w-full border-r-4 border-white'
+                                    initial={{ height: 0 }}
+                                    whileInView={{ height: 30 }}
+                                    transition={{
+                                        type: 'spring',
+                                        stiffness: 350,
+                                        damping: 18,
+                                    }}
+                                ></motion.div>
+                            )}
+                            <TbSortZA className='text-white' fontSize={32} />
+                        </div>
+                    </Tooltip>
+                    <Tooltip closeDelay={0} placement='left' offset={-7} className='text-xs font-bold' content='Playtime High to Low'>
+                        <div className='relative flex justify-center items-center w-full h-[62px] hover:bg-sgi cursor-pointer' onClick={() => setSortStyle('1-0')}>
+                            {sortStyle === '1-0' && (
+                                <motion.div
+                                    className='absolute w-full border-r-4 border-white'
+                                    initial={{ height: 0 }}
+                                    whileInView={{ height: 30 }}
+                                    transition={{
+                                        type: 'spring',
+                                        stiffness: 350,
+                                        damping: 18,
+                                    }}
+                                ></motion.div>
+                            )}
+                            <MdOutlineTimer10Select className='text-white' fontSize={24} />
+                        </div>
+                    </Tooltip>
+                    <Tooltip closeDelay={0} placement='left' offset={-7} className='text-xs font-bold' content='Playtime Low to High'>
+                        <div className='relative flex justify-center items-center w-full h-[62px] hover:bg-sgi cursor-pointer' onClick={() => setSortStyle('0-1')}>
+                            {sortStyle === '0-1' && (
+                                <motion.div
+                                    className='absolute w-full border-r-4 border-white'
+                                    initial={{ height: 0 }}
+                                    whileInView={{ height: 30 }}
+                                    transition={{
+                                        type: 'spring',
+                                        stiffness: 350,
+                                        damping: 18,
+                                    }}
+                                ></motion.div>
+                            )}
+                            <MdOutlineTimer3Select className='text-white' fontSize={24} />
+                        </div>
+                    </Tooltip>
+                    <Tooltip closeDelay={0} placement='left' offset={-7} className='text-xs font-bold' content='Recently Played'>
+                        <div className='relative flex justify-center items-center w-full h-[62px] hover:bg-sgi cursor-pointer' onClick={() => setSortStyle('recent')}>
+                            {sortStyle === 'recent' && (
+                                <motion.div
+                                    className='absolute w-full border-r-4 border-white'
+                                    initial={{ height: 0 }}
+                                    whileInView={{ height: 30 }}
+                                    transition={{
+                                        type: 'spring',
+                                        stiffness: 350,
+                                        damping: 18,
+                                    }}
+                                ></motion.div>
+                            )}
+                            <MdAvTimer className='text-white' fontSize={24} />
+                        </div>
+                    </Tooltip>
+                    <Tooltip closeDelay={0} placement='left' offset={-7} className='text-xs font-bold' content='Favorited'>
+                        <div className='relative flex justify-center items-center w-full h-[62px] hover:bg-sgi cursor-pointer' onClick={() => setSortStyle('fav')}>
+                            {sortStyle === 'fav' && (
+                                <motion.div
+                                    className='absolute w-full border-r-4 border-white'
+                                    initial={{ height: 0 }}
+                                    whileInView={{ height: 30 }}
+                                    transition={{
+                                        type: 'spring',
+                                        stiffness: 350,
+                                        damping: 18,
+                                    }}
+                                ></motion.div>
+                            )}
+                            <TiHeartFullOutline className='text-white' fontSize={24} />
+                        </div>
+                    </Tooltip>
                 </div>
 
                 <div className='flex justify-center items-center'>
